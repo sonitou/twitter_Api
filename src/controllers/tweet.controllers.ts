c0ba5764d1945e22f9222eddda2fea6006c35b87
sonn30502 <sonn220619@gmail.com>
2025-03-04 15:17:26 +0700

    Tạo Tweet

--- src/controllers/tweet.controllers.ts
+++ src/controllers/tweet.controllers.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from 'express'
 import { TweetRequestBody } from '~/models/requests/Tweet.request'
 import { ParamsDictionary } from 'express-serve-static-core'
+import { TokenPayload } from '~/models/requests/User.requests'
+import tweetsService from '~/services/tweets.services'
 
 export const createTweetController = async (req: Request<ParamsDictionary, any, TweetRequestBody>, res: Response) => {
-  res.send('Ok')
+  const { user_id } = req.decoded_authorization as TokenPayload
+  const result = await tweetsService.createTweet(user_id, req.body)
+  res.json({
+    message: 'Create Tweet Successfully',
+    result
+  })
 }
--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -3,6 +3,7 @@ import { config } from 'dotenv'
 import User from '~/models/schemas/User.schemas'
 import RefreshToken from '~/models/schemas/RefreshToken.schemas'
 import Follower from '~/models/schemas/Follower.schemas'
+import Tweet from '~/models/schemas/Tweet.schemas'
 config()
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@twitter.qwzev.mongodb.net/?retryWrites=true&w=majority&appName=twitter`
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -74,6 +75,10 @@ class DatabaseService {
 
   get followers(): Collection<Follower> {
     return this.db.collection(process.env.DB_FOLLOWER_COLLECTION as string)
+  }
+
+  get tweets(): Collection<Tweet> {
+    return this.db.collection(process.env.DB_TWEET_COLLECTION as string)
   }
 }
 
